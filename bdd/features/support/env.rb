cf7c04c9a3a0154cf2ef34e30426ba22bb660e1c
Mauricio Silva <mauricio.silva@chaordicsystems.com>
2015-06-18 23:02:55 -0300

    add support to functional testing

--- bdd/features/support/env.rb
+++ bdd/features/support/env.rb
@@ -1,14 +1,18 @@
-require 'capybara/cucumber'
-require 'capybara/poltergeist'
-
-if ENV["HEADLESS"] then
-    Capybara.register_driver :poltergeist do |app|
-        Capybara::Poltergeist::Driver.new(app, window_size: [1280, 1024], debug: true)
-    end
-    Capybara.default_driver    = :poltergeist
-    Capybara.javascript_driver = :poltergeist
+if ENV["FUNCTIONAL"] then
+    Dir["./steps/functional/*.rb"].each {|file| require file }
 else
-    Capybara.default_driver = :selenium
+    require 'capybara/cucumber'
+    require 'capybara/poltergeist'
+    Dir["./steps/web/*.rb"].each {|file| require file }
+    if ENV["HEADLESS"] then
+        Capybara.register_driver :poltergeist do |app|
+            Capybara::Poltergeist::Driver.new(app, window_size: [1280, 1024], debug: true)
+        end
+        Capybara.default_driver    = :poltergeist
+        Capybara.javascript_driver = :poltergeist
+    else
+        Capybara.default_driver = :selenium
+    end
+    Capybara.default_selector = :css
 end
-Capybara.default_selector = :css
  
---
+++ bdd/steps/functional/first.rb
@@ -0,0 +1,7 @@
+Given(/^a search website$/) do
+    puts "oi"
+end
+
+When(/^I query for Naldo$/) do
+    puts "tchau"
+end
