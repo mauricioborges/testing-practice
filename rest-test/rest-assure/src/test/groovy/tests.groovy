f4774126317830cfc835d2810831d5f311f65ea8
Maurício Borges Silva <mauricioborges@gmail.com>
2015-07-06 10:57:55 -0300

    first test working with REST assured

--- rest-test/rest-assure/src/test/groovy/tests.groovy
+++ rest-test/rest-assure/src/test/groovy/tests.groovy
@@ -1,8 +1,17 @@
-import org.junit.Test
+import static com.jayway.restassured.RestAssured.*
+import static com.jayway.restassured.matcher.RestAssuredMatchers.*
+import static org.hamcrest.Matchers.*
 
 class MyClassTest extends GroovyTestCase {
 
-    void "testa se consigo obter um token para operar a API"() {
-        assert false
+    void setUp(){
+        baseURI = "http://correiosapi.apphb.com"
+    }
+    
+    void "testa se consigo obter um endereço a partir de um CEP"() {
+        get("/cep/{cep}", 88062000)
+        .then()
+        .assertThat()
+        .body("logradouro", equalTo("Afonso Dalembert"))
     }
 }
